0a8b59507578ff785ec229bd13f1a54f5d96d1de
unknown <linyiquan@dm-ai.cn>
2020-03-20 10:33:02 +0800

    style: reorganize files

--- seq2seq/generate_samples.sh
+++
@@ -1,1 +0,0 @@
-python generate_samples.py --cuda --model trs --batch_size 1 --use_sgd --lr 0.01 --meta_lr 0.0003 --meta_batch_size 16 --meta_optimizer adam --pretrain_emb --weight_sharing --emb_dim 300 --hidden_dim 300 --save_path save/no_persona/model_4_7.2880_0.0000_0.0000_0.0000_1.0000_pointer --save_path_dataset save/no_persona/ --test --pointer_gen --read_renmin
--- seq2seq/main_trs_baseline.sh
+++
@@ -1,1 +0,0 @@
-python main.py --cuda --model trs --pretrain_emb --weight_sharing --label_smoothing --noam --emb_dim 300 --hidden_dim 300 --save_path save/no_persona/ --pointer_gen
